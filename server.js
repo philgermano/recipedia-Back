c0770423f11080ea4ba7bfe354b98ec5087102b4
clara-lima <claracmelo@hotmail.com>
2022-09-21 19:14:18 -0700

    postman works

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 // whitelist & corsOptions
-// const whitelist = ['http://localhost:3000', 'https://fathomless-sierra-68956.herokuapp.com']
+const whitelist = ['http://localhost:3000', '*']
 const corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
+    // if (whitelist.indexOf(origin) !== -1) {
+    //   callback(null, true)
+    // } else {
+    //   callback(new Error('Not allowed by CORS'))
+    // }
+    callback(null, true)
   }
 }
 
