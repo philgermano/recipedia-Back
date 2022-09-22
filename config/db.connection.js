7c0283fbfed8e0e90d5e764257b44c0a1b26c2c7
clara-lima <claracmelo@hotmail.com>
2022-09-21 17:38:29 -0700

    mongoDB added

--- config/db.connection.js
+++ config/db.connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionStr = process.env.MONGODB_URI || 'mongodb://localhost:27017/recepediaDB'
+const connectionStr = process.env.MONGODB_URI 
 
 mongoose.connect(connectionStr);
 
