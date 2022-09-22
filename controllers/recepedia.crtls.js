038e10a71fe5d5268df01564ae34355526232c1b
clopezferrer <lopezferrer@gmail.com>
2022-09-21 20:18:44 -0400

    controllers directory

---
+++ controllers/recepedia.crtls.js
@@ -0,0 +1,52 @@
+const db = require('../models');
+
+const index = (req, res) => {
+  db.Recipe.find({}, (error, recipes) => {
+    if(error) return res.status(400).json({ error: error.message });
+
+    return res.status(200).json({
+      recipes,
+      requestedAt: new Date().toLocaleString()
+    });
+  });
+};
+
+const create = (req, res) => {
+  db.Recipe.create(req.body, (error, createdRecipe) => {
+    if(error) return res.status(400).json({ error: error.message });
+
+    return res.status(200).json(createdRecipe); //  .json() will send proper headers in response so client knows it's json coming back
+  });
+};
+
+const destroy = (req, res) => {
+  db.Recipe.findByIdAndDelete(req.params.id, (error, deletedRecipe) => {
+    if(error) return res.status(400).json({ error: error.message });
+
+    return res.status(200).json({
+      message: `${deletedRecipe.name} deleted successfully`
+    });
+  });
+};
+
+const update = (req, res) => {
+  db.Recipe.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body
+    },
+    { new: true },
+    (error, updatedRecipe) => {
+    if(error) return res.status(400).json({ error: error.message });
+
+    return res.status(200).json(updatedRecipe)
+  });
+};
+
+
+module.exports = {
+    index,
+    create,
+    destroy,
+    update,
+}
